a8e40160982eee85612e4de3af9129ceed10e6a5
Miister95 <cairne_abraham@hotmail.com>
2023-09-20 19:23:40 -0500

    Agregando demojpa semana 03

---
+++ Semana03_DemoJPA/EjemploJPA/src/main/java/lp2/cibertec/service/EncontrarEmpleado.java
@@ -0,0 +1,26 @@
+package lp2.cibertec.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import lp2.cibertec.entidades.Employee;
+
+public class EncontrarEmpleado {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emfactory = 
+				Persistence.createEntityManagerFactory("EjemploJPA");
+		
+		EntityManager entitymanager = emfactory.createEntityManager();
+		entitymanager.getTransaction().begin();
+		
+		Employee employee = entitymanager.find(Employee.class, 1201);
+		
+		System.out.println("Id Empleado: " + employee.getEid());
+		System.out.println("Nombre de Empleado: " + employee.getEname());
+		System.out.println("Salario de Empleado: " + employee.getSalary());
+		System.out.println("Cargo del Empleado: " + employee.getDeg());
+	}
+
+}
---
+++ Semana03_DemoJPA/EjemploJPA/src/main/java/lp2/cibertec/service/FuncionesAgregadoEscalar.java
@@ -0,0 +1,34 @@
+package lp2.cibertec.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+
+public class FuncionesAgregadoEscalar {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emfactory = 
+				Persistence.createEntityManagerFactory("EjemploJPA");
+		
+		EntityManager entitymanager = emfactory.createEntityManager();
+		
+		//Funcion escalar
+		Query query = entitymanager.createQuery("Select UPPER(e.ename) from Employee e");
+		
+		List<String> list = query.getResultList();
+		
+		for(String e : list) {
+			System.out.println("Nombre del empleado" + e);
+		}
+		
+		//Funcion de agregado
+		Query query1 = entitymanager.createQuery("Select MAX(e.Salary) from Employee e");
+		Double result = (Double) query1.getSingleResult();
+		System.out.println("Maximo salario del empleado: " + result);
+	}
+
+}
